d5089aff79816f4b194e9d5e05e6ede4c9062366
Serhii Kolesnyk <serhii.kolesnyk.external@bonial.com>
2020-11-24 21:22:02 +0200

    lab1

---
+++ lab1/lab1.lib/CustomString.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Nau.Lib
+{
+    public class CustomString
+    {
+
+        private int[] value;
+
+        public CustomString(int[] value)
+        {
+            this.value = value;
+        }
+
+        public int[] Value()
+        {
+            return value;
+        }
+
+        public int Size()
+        {
+            return value.Length;
+        }
+
+        ~CustomString()
+        {
+            this.value = null;
+            Console.WriteLine($"The {ToString()} destructor is executing.");
+        }
+
+        public void Reverse()
+        {
+            int temp;
+            int start = 0;
+            int end = Size() - 1;
+
+            while (start < end)
+            {
+                temp = value[start];
+                value[start] = value[end];
+                value[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+    }
+}
---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Nau.Lib;
+
+namespace Nau.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var example = new CustomString(new int[] { 1, 2, 3, 4, 5 });
+            var size = example.Size();
+            Console.WriteLine("CustomString size: " + size);
+
+            PrintCustoStringValue(example);
+
+            Console.WriteLine("CustomString value after reverse.");
+            example.Reverse();
+
+            PrintCustoStringValue(example);
+        }
+
+        private static void PrintCustoStringValue(CustomString example)
+        {
+            Console.Write("CustomString value: ");
+
+            foreach (var item in example.Value())
+            {
+                Console.Write(item.ToString());
+            }
+            Console.Write("\n");
+        }
+    }
+}
